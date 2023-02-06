3c5c8392c4cc6125c6a4f5a4a68367b4881f0f40
Gwenaelle <75257467+gwendvr@users.noreply.github.com>
2023-02-06 14:25:58 +0100

    map

--- SFML Projet 1/SFML Projet 1/Main.cpp
+++ SFML Projet 1/SFML Projet 1/Main.cpp
@@ -1,68 +1,10 @@
 #include <SFML/Graphics.hpp>
 
-void spawnRectangles(sf::RenderWindow& window, std::vector<sf::RectangleShape>& rectangles, float spawnInterval) {
-    float spawnTimer = 0.f;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        spawnTimer += 1.f / 60.f;
-
-        if (spawnTimer >= spawnInterval) {
-            spawnTimer = 0.f;
-
-            sf::RectangleShape rect;
-            rect.setSize(sf::Vector2f(10, 50));
-            rect.setFillColor(sf::Color::White);
-            rect.setPosition(rand() % 800, -50);
-            rectangles.push_back(rect);
-        }
-
-        for (auto it = rectangles.begin(); it != rectangles.end();) {
-            it->move(0, 5);
-
-            if (it->getPosition().y > 600) {
-                it = rectangles.erase(it);
-            }
-            else {
-                ++it;
-            }
-        }
-
-        window.clear();
-
-        for (const auto& rect : rectangles) {
-            window.draw(rect);
-        }
-
-        window.display();
-    }
-}
-
 int main()
 {
-
-    std::vector<sf::RectangleShape> rectangles;
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Barrière");
-    window.setFramerateLimit(60);
-
-    //Création de la barrière rouge
-    sf::RectangleShape barriere(sf::Vector2f(100, 15));
-    barriere.setFillColor(sf::Color::Red);
-    barriere.setPosition(50, 363);
-
-    //Création des pieds en béton gris
-    sf::RectangleShape pied1(sf::Vector2f(15, 40));
-    sf::RectangleShape pied2(sf::Vector2f(15, 40));
-    pied1.setFillColor(sf::Color(128, 128, 128));
-    pied2.setFillColor(sf::Color(128, 128, 128));
-    pied1.setPosition(35, 350);
-    pied2.setPosition(135, 350);
+    sf::RenderWindow window(sf::VideoMode(390, 650), "SFML Window");
+    sf::RectangleShape background(sf::Vector2f(390, 650));
+    background.setFillColor(sf::Color(80,80,80));
 
     while (window.isOpen())
     {
@@ -74,14 +16,22 @@ int main()
         }
 
         window.clear();
-        window.draw(pied1);
-        window.draw(pied2);
-        window.draw(barriere);
-        spawnRectangles(window, rectangles, 0.1f);
+
+        sf::VertexArray lines(sf::Lines, 4);
+        lines[0].position = sf::Vector2f(130, 0);
+        lines[0].color = sf::Color::White;
+        lines[1].position = sf::Vector2f(130, 650);
+        lines[1].color = sf::Color::White;
+        lines[2].position = sf::Vector2f(260, 0);
+        lines[2].color = sf::Color::White;
+        lines[3].position = sf::Vector2f(260, 650);
+        lines[3].color = sf::Color::White;
+
+        window.draw(background);
+        window.draw(lines);
         window.display();
     }
 
     return 0;
 }
-
 
